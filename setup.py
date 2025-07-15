9141d55e881ab251b8334989b78799c89f6f0bca
jpaganini <paganinijulian@gmail.com>
2025-07-15 13:35:19 +0200

    changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="maGeneLearn",
-    version="0.1.0",
+    version="0.1.1",
     description="A CLI wrapper for the maGeneLean ML pipeline",
     author="Julian A. Paganini",
     author_email="j.a.paganini@uu.nl",
